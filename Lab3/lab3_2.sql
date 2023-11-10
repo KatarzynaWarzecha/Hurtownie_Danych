db6dabefc2cfd1aa177da8752761ca096ff61b36
KatarzynaWarzecha <104493623+KatarzynaWarzecha@users.noreply.github.com>
2023-11-10 21:37:04 +0100

    sql with outputs add

---
+++ Lab3/lab3_2.sql
@@ -0,0 +1,10 @@
+--CREATE DATABASE PersonZad66
+--COLLATE SQL_Latin1_General_CP1_CI_AS
+
+use PersonZad66
+INSERT INTO [Person.Person] ([BusinessEntityID], [rowguid], [ModifiedDate],
+[PersonType], [NameStyle], [Title], [FirstName], [MiddleName], [LastName], [Suffix],
+[EmailPromotion], [AdditionalContactInfo], [Demographics])
+VALUES (20880, '92C4279F-1207-48A3-8448-4636514EB7E3', CURRENT_TIMESTAMP, 'EM', 0,
+'Mr.', 'Chris', 'Albert', 'White', null, 0, null, '<IndividualSurvey
+xmlns="http://schemas.microsoft.com/sqlserver/2004/07/adventureworks/IndividualSurvey"><TotalPurchaseYTD>0</TotalPurchaseYTD></IndividualSurvey>')
